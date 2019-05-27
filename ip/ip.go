7c8b6a647e0357b5ba64aa91719806bccaac84e1
Hemant Mann <hemant.mann121@gmail.com>
2019-05-27 19:03:00 +0530

    feat(maskIP method)

--- ip/ip.go
+++ ip/ip.go
@@ -2,6 +2,7 @@ package ip
 
 import (
 	"net"
+	"strings"
 )
 
 // IsInternalIP method checks whether the IP supplied is internal
@@ -11,4 +12,26 @@ func IsInternalIP(ip string) bool {
 	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
 	_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
 	return private24BitBlock.Contains(ipaddr) || private20BitBlock.Contains(ipaddr) || private16BitBlock.Contains(ipaddr)
+}
+
+// MaskIP removes the last octet from the IP address if it is a valid IP address
+func MaskIP(ip string) string {
+	var ipaddr = net.ParseIP(ip)
+	var ipv4 = ipaddr.To4()
+	var ipv6 = ipaddr.To16()
+	if ipv4 != nil {
+		var parts = strings.Split(ip, ".")
+		var length = len(parts)
+		parts[length-1] = "0"
+
+		ip = strings.Join(parts, ".")
+	} else if ipv6 != nil {
+		var parts = strings.Split(ip, ":")
+		var length = len(parts)
+		parts[length-1] = "0000"
+
+		ip = strings.Join(parts, ":")
+	}
+
+	return ip
 }
