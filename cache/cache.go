c80f4f9012604454f47526bbf45208ae38e4a574
Hemant Mann <hemant.mann121@gmail.com>
2020-04-09 14:52:15 +0530

    feat(multi cache client)

--- cache/cache.go
+++ cache/cache.go
@@ -1,14 +1,23 @@
 package cache
 
 import (
+	"encoding/json"
 	"time"
 
+	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/patrickmn/go-cache"
 )
 
 type Client struct {
 	prefix string
 	client *cache.Cache
+}
+
+type MultiClient struct {
+	prefix     string
+	expiration int
+	client     *cache.Cache
+	mc         *memcache.Client
 }
 
 func NewClient(prefix string, defCacheTime int) *Client {
@@ -33,4 +42,55 @@ func (cc *Client) Get(key string) (interface{}, bool) {
 
 func (cc *Client) Delete(key string) {
 	cc.client.Delete(cc.getKeyName(key))
+}
+
+func NewMultiClient(prefix, mcServer string, defCacheTime int) *MultiClient {
+	var cacheTime = time.Duration(defCacheTime) * time.Minute
+	c := cache.New(cacheTime, 10*time.Minute)
+	mc := memcache.New(mcServer)
+
+	var cc = &MultiClient{client: c, mc: mc, prefix: prefix, expiration: defCacheTime * 60}
+	return cc
+}
+
+func (cc *MultiClient) getKeyName(key string) string {
+	return cc.prefix + "_" + key
+}
+
+func (cc *MultiClient) Set(key string, val interface{}) {
+	k := cc.getKeyName(key)
+	cc.client.SetDefault(k, val)
+
+	result, err := json.Marshal(val)
+	if err == nil {
+		cc.mc.Set(&memcache.Item{
+			Key:        k,
+			Value:      result,
+			Expiration: int32(cc.expiration),
+		})
+	}
+}
+
+func (cc *MultiClient) Get(key string) (interface{}, bool) {
+	k := cc.getKeyName(key)
+	result, found := cc.client.Get(k)
+	if found {
+		return result, found
+	}
+	item, err := cc.mc.Get(k)
+	if err == nil {
+		var cacheObj interface{}
+		err = json.Unmarshal(item.Value, cacheObj)
+		if err == nil {
+			return cacheObj, true
+		}
+	}
+
+	return nil, false
+}
+
+func (cc *MultiClient) Delete(key string) {
+	k := cc.getKeyName(key)
+	cc.client.Delete(k)
+	cc.mc.Delete(k)
 }
